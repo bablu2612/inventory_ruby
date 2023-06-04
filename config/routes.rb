17341ac5b82f8c61b2c4320867009c5eb3510e4d
Satish <satynautiyal@gmail.com>
2023-06-04 16:27:43 +0530

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :car_details
+  root "car_details#index"
   get 'home/index', to: "home#index"
   get '/single_car', to: "home#single_car"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
