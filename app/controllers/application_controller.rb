0fb1b57602af32a16548c27a08de3e2873040295
Satish <satynautiyal@gmail.com>
2023-06-05 22:33:43 +0530

    added devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+        "/admin"
+    end
 end
