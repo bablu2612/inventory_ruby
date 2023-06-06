e528d402ec300bfbd87611177e3818362905c46c
Satish <satynautiyal@gmail.com>
2023-06-07 01:46:33 +0530

    added purchase inquiry modal

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,7 +1,8 @@
 class OfferMailer < ApplicationMailer
 
-    def send_offer_email(car)
+    def send_offer_email(car, params)
         @car = car
+        @params = params
 
         emails = ['roy@inpublic.com','kevin@inpublic.com','james@inpublic.com','bk147411@gmail.com']
     #    emails = ['bk147411@gmail.com','bablu@csgroupchd.com']
